e472db41122b8e108fade50ab613a768181c70bf
ankitsejwal <ankit@sejw.al>
2020-09-09 19:51:38 +1000

    primitive data types

---
+++ datatypes/Primitive.java
@@ -0,0 +1,17 @@
+class Primitive{
+    public static void main(String[] args) {
+    
+        // popular primitive data types
+
+        int     age     = 12;
+        double  value   = 13.5;
+        char    initial = 'A';
+        boolean isNight = true;
+
+        System.out.println(age);
+        System.out.println(value);
+        System.out.println(initial);
+        System.out.println(isNight);
+
+    } 
+}
