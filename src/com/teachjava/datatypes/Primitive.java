cec8d5cba1bb58d5bd66c54586238227649f9b79
ankitsejwal <ankit@sejw.al>
2020-09-09 21:16:52 +1000

    input

---
+++ src/com/teachjava/datatypes/Primitive.java
@@ -0,0 +1,19 @@
+package com.teachjava.datatypes;
+
+public class Primitive {
+    public static void main(String[] args) {
+
+        // popular primitive data types
+
+        int     age     = 12;       // whole number
+        double  value   = 13.5;     // decimal number
+        char    initial = 'A';      // single character
+        boolean isNight = true;     // true or false value
+
+        System.out.println(age);
+        System.out.println(value);
+        System.out.println(initial);
+        System.out.println(isNight);
+
+    }
+}
---
+++ src/com/teachjava/datatypes/ReferenceTypes.java
@@ -0,0 +1,35 @@
+package com.teachjava.datatypes;
+
+import java.util.Scanner;
+
+public class ReferenceTypes {
+    public static void main(String[] args) {
+
+        // String is the most popular Reference type datatype
+
+        // Syntax: String StringName = GiveValueinQuotes;
+
+        String firstName        = "Jacob";
+        String lastName         = "Black";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        // charAt(index) method is used to get a character out of a string
+        char   firstNameInitial = firstName.charAt(0);
+        char   lastNameInitial  = lastName.charAt(0);
+
+        System.out.println(firstNameInitial);
+        System.out.println(lastNameInitial);
+
+        // concatenation
+        System.out.println("The full name is " + firstName + " " + lastName);
+
+        // inputs through command line
+        System.out.println("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+
+        // read input
+        System.out.println(input.nextLine());
+    }
+}
