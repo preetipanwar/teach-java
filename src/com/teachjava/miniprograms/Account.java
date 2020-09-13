95e30e1731a19c77319798e6ccde7a6fa58508a3
ankitsejwal <ankit@sejw.al>
2020-09-14 01:02:59 +1000

    Instructions for miniprograms

--- src/com/teachjava/miniprograms/Account.java
+++ src/com/teachjava/miniprograms/Account.java
@@ -1,4 +1,19 @@
 package com.teachjava.miniprograms;
 
 public class Account {
+
+// features -
+// deposit money in bank
+// cash out money from bank
+// see bank balance
+// transfer money between bank accounts
+
+
+// field -> balance
+    // constructor
+    // printBalance()   - void
+    // debit()          - int
+    // credit()         - int
+    // transaction()    - int
+
 }
--- src/com/teachjava/miniprograms/Movie.java
+++ src/com/teachjava/miniprograms/Movie.java
@@ -1,4 +1,16 @@
 package com.teachjava.miniprograms;
 
 public class Movie {
+
+    // Field every movie should hold
+        // movieName    - String
+        // actors       - [actor1, actor2, actor3]
+        // releaseDate  - Date
+        // duration     - int       -- duration in minutes
+
+
+    // Methods()
+        // printMovieName()
+        // printActors()
+        // printDuration()
 }
