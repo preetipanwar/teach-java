6a6ff781a2db67d6f385a0b91b7a3c9ae15fca58
preeti panwar <preetipanwar102@gmail.com>
2020-09-16 23:35:59 +0530

    Implementation of Account and Movie class

--- src/com/teachjava/miniprograms/Account.java
+++ src/com/teachjava/miniprograms/Account.java
@@ -9,11 +9,52 @@ public class Account {
 // transfer money between bank accounts
 
 
-// field -> balance
+// field -> balance     - int
     // constructor
     // printBalance()   - void
     // debit()          - int
     // credit()         - int
-    // transaction()    - int
+    // transfer()       - int           -> passing object as variable
 
+    int balance;
+
+    Account(int balance){
+        this.balance = balance;
+    }
+
+    void printBalance(){
+        System.out.println(balance);
+    }
+
+    int debit(int amount){
+         balance = this.balance - amount;
+         printBalance();
+         return balance;
+    }
+
+    int credit(int amount){
+         balance = this.balance + amount;
+         printBalance();
+         return balance;
+    }
+
+    // passing object as a parameter ()
+    void transfer(Account account, int amount){
+        // currentObject - amount debit
+        debit(amount);
+        // account - amount credit
+        account.credit(amount);
+    }
+
+    public static void main(String[] args) {
+        Account dad = new Account(50000);
+        Account mom = new Account(40000);
+
+//        dad.printBalance();
+//        dad.debit(5000);
+//        dad.credit(10000);
+
+        dad.transfer(mom, 5000);
+    }
 }
+
--- src/com/teachjava/miniprograms/Movie.java
+++ src/com/teachjava/miniprograms/Movie.java
@@ -2,15 +2,52 @@ package com.teachjava.miniprograms;
 
 public class Movie {
 
+
     // Field every movie should hold
         // movieName    - String
         // actors       - [actor1, actor2, actor3]
         // releaseDate  - Date
         // duration     - int       -- duration in minutes
 
+     String movieName;
+     String actor;
+
+    public Movie(String movieName, String actor){
+        this.movieName = movieName;
+        this.actor = actor;
+    }
+
 
     // Methods()
         // printMovieName()
         // printActors()
         // printDuration()
+
+
+
+    public static void main(String[] args) {
+
+
+
+        // In Java, variables cannot contain objects, they only references to objects.
+        Movie taarezameenpar = new Movie("taarezameenpar", "aamir"); // --> memory address 00x12123
+
+        Movie likestartsinthesky = taarezameenpar;                                     // --> memory address 00x12123
+
+        taarezameenpar.actor = "sharman joshi";
+        System.out.println(taarezameenpar.actor);
+        System.out.println(likestartsinthesky.actor);
+
+    }
 }
+
+
+
+// 1. this keyword
+// 2. object reference variable -> Movie class
+// 3. object as a parameter -> Account class
+// 4. Garbage collection
+
+// 5. finalize vs final -> internet
+
+
