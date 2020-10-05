e7e19e4020223f46719121eef62c6dc04365b49a
ankitsejwal <ankit@sejw.al>
2020-10-05 20:36:11 +1100

    Print all prime numbers till a given input

--- src/com/teachjava/labs/PrimeNumber.java
+++ src/com/teachjava/labs/PrimeNumber.java
@@ -3,34 +3,26 @@ package com.teachjava.labs;
 public class PrimeNumber {
     public static void main(String[] args) {
 
-//        int dividend = 10;
-//        int divisor = 17;
-//
-//        int quotient = dividend / divisor;
-//        int remainder = dividend % divisor;
-//
-//        System.out.println(quotient);
-//        System.out.println(remainder);
+        int divisor = 2;
+        int dividend = 6;
 
-
-        int dividend = 7;
-
-        for (int divisor = 2; divisor <= dividend; divisor++){
-            System.out.println("divisor " + divisor);
+        while (divisor <= dividend){
+            System.out.println(divisor);
             int quotient = dividend / divisor;
             int remainder = dividend % divisor;
 
-            if (quotient > 1 && remainder == 0) {
-                // it means dividends has factors
-                break;          // break the loop and go to next number
-                }
-
-                System.out.println(dividend);
-
-
-
-
+            // check if the dividend has factors
+            if (remainder == 0 && quotient > 1){
+                // thus is not prime break the loop
+                System.out.println("dividend " + dividend + " is not a prime number");
+                break;
             }
+
+            if (divisor == dividend){
+                System.out.println(divisor + " is Prime");
+            }
+            divisor++;              // check for the next
+        }
 
         }
 
---
+++ src/com/teachjava/labs/PrimeNumberInput.java
@@ -0,0 +1,37 @@
+package com.teachjava.labs;
+import java.util.Scanner;
+
+public class PrimeNumberInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Print prime numbers upto: ");
+        int searchLimit =  input.nextInt();
+
+        for (int dividend = 2; dividend <= searchLimit; dividend++){
+            int divisor = 2;
+
+            while (divisor <= dividend){
+                int quotient = dividend / divisor;
+                int remainder = dividend % divisor;
+
+                // check if the dividend has factors
+                if (remainder == 0 && quotient > 1){
+                    // thus is not prime break the loop
+                    System.out.println(dividend + " is not a prime number");
+                    break;
+                }
+
+                if (divisor == dividend){
+                    System.out.println(divisor + " is Prime");
+                }
+                divisor++;              // check for the next
+            }
+
+        }
+        }
+
+
+
+}
+
