a1f7c16694d4137f0aa298104f79e4a91e0e684c
preeti panwar <preetipanwar102@gmail.com>
2020-10-05 15:40:27 +0530

    get input from user

--- src/com/teachjava/labs/PrimeNumber.java
+++ src/com/teachjava/labs/PrimeNumber.java
@@ -1,14 +1,19 @@
 package com.teachjava.labs;
+import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
 
         int divisor = 2;
-        int dividend = 6;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number to see if its Prime:  ");
+        int dividend = input.nextInt();
 
         while (divisor <= dividend){
-            int quotient = dividend / divisor;
-            int remainder = dividend % divisor;
+            int quotient = dividend / divisor;  // 5
+            int remainder = dividend % divisor; // 0
+            // dividend = divisor x quotient + remainder
 
             // check if the dividend has factors
             if (remainder == 0 && quotient > 1){
@@ -17,6 +22,7 @@ public class PrimeNumber {
                 break;
             }
 
+            // check if loop reached the last number
             if (divisor == dividend){
                 System.out.println(divisor + " is Prime");
             }
@@ -24,3 +30,22 @@ public class PrimeNumber {
         }
     }
 }
+
+
+
+
+// dividend 6
+// divisor 2
+
+// quotient 3
+// remainder 0
+
+// dividend = divisor x quotient + remainder
+// 6 = 2 x 3 + 0
+// 8 = 2 x 2 + 0
+// 11 = 2 x 5 + 1
+// 11 = 11 x 1 + 0
+// 13 = 3 x 4 + 1
+
+// Prime - divisible by self and 1
+// Factor
