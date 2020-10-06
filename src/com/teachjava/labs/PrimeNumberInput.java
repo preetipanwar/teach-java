436ea7e11c343b3b26ec244c9c5a87121653da95
ankitsejwal <ankit@sejw.al>
2020-10-06 11:18:33 +1100

    switched for while loop to for loop and added more comments

--- src/com/teachjava/labs/PrimeNumberInput.java
+++ src/com/teachjava/labs/PrimeNumberInput.java
@@ -5,30 +5,37 @@ public class PrimeNumberInput {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Print prime numbers upto: ");
-        int searchLimit =  input.nextInt();
+        System.out.println("Print prime numbers up-to: ");
+        int lastDividend =  input.nextInt();
 
-        for (int dividend = 2; dividend <= searchLimit; dividend++){
+        // loop through all the dividends
+        for (int dividend = 2; dividend <= lastDividend; dividend++){
 
-            int divisor = 2;        // initial value
-
-            while (divisor <= dividend){
+            // Check if a dividend is Prime
+            for (int divisor = 2; divisor <= dividend; divisor++){
                 int quotient = dividend / divisor;
                 int remainder = dividend % divisor;
 
                 // check if the dividend has factors
+                // formula: dividend    = divisor   x quotient  + remainder
+                // e.g   :     12          = 2         x 6         + 0
+                // Here quotient is greater than 1 hence the number 12 can be
+                // divided into smaller parts (factors)
+
                 if (remainder == 0 && quotient > 1){
-                    // thus is not prime break the loop
-                    System.out.println(dividend + " is not a prime number");
+                    // Not prime break the loop and go to the next dividend
                     break;
                 }
 
+                // if dividend cannot be divided into any factors and loop
+                // reaches the final value which will surely divide the number
+                // by itself, then its a Prime number.
                 if (divisor == dividend){
-                    System.out.println(divisor + " is Prime");
+                    System.out.println(dividend + " is Prime");
                 }
-                divisor++;              // check for the next
-            }
-        }
-    }
-}
+            } // end internal loop
+        } // end for loop for looping all the divisors one by one
 
+
+    } // end main()
+} // end Class
