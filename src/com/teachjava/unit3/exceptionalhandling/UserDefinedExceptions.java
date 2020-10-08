d85600742d694ccfb73b2cc8f0a26e0d51833fed
ankitsejwal <ankit@sejw.al>
2020-10-08 13:47:15 +1100

    userdefined exceptions

---
+++ src/com/teachjava/unit3/exceptionalhandling/UserDefinedExceptions.java
@@ -0,0 +1,13 @@
+package com.teachjava.unit3.exceptionalhandling;
+
+public class UserDefinedExceptions extends Exception{
+    // you can create your own exceptions in Java
+    // to create your own exceptions you need to extend
+    // the predefined class Exception
+
+//    class MyException extends Exception{
+//
+//    }
+//    An exception class is like any other class,
+//    containing useful fields and methods.
+}
