a757fdf4c6bab9191c7fa6020c033072c6145643
ankitsejwal <ankit@sejw.al>
2020-10-08 12:42:16 +1100

    Handling multiple exceptions

--- src/com/teachjava/unit3/exceptionalhandling/Handling.java
+++ src/com/teachjava/unit3/exceptionalhandling/Handling.java
@@ -1,4 +1,5 @@
 package com.teachjava.unit3.exceptionalhandling;
+import java.lang.Exception;
 
 public class Handling {
 
@@ -29,10 +30,49 @@ public class Handling {
 
         // Run the program, the exception will be printed from catch block
         // and the program will keep on running.
+    }
+}
 
-        // ---------------------------------------------------------------- //
+class HandlingMultipleExceptions{
+    public static void main(String[] args) {
+        // Catching multiple exceptions
 
+        // A try block can be followed by multiple catch blocks.
+        // The syntax for multiple catch blocks looks like the following:
 
+//        try {
+//            Protected code
+//        } catch (ExceptionType1 e) {
+//            Catch block
+//        } catch (ExceptionType2 e) {
+//            Catch block
+//        } catch (ExceptionType3 e) {
+//            Catch block
+//        }
+
+//        You can have any number of catch blocks after a single try.
+//        If an exception occurs in try block the consecutive catch block will try to handle it
+//        one by one. If the above catch block cannot handle the exception the next catch block
+//        will try to handle it, if not it will again pass it down to the next catch block.
+
+        try{
+            int num[]= {1, 2, 3, 4};
+            num[5]=30/0;
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBounds Exception occurs");
+        }
+        catch(ArithmeticException e){
+            System.out.println("Arithmetic Exception occurs");
+        }
+        catch(Exception e){
+            System.out.println("Parent Exception occurs");
+        }
+
+//        In the example above 30/0 will result into Arithmetic Exception. To handle it the first
+//        catch block will try to handle it, since it cannot deal with this exception it will pass
+//        it down to the next catch block now this block can handle ArithmeticException and hence can
+//        prevent the program from failing.
     }
 }
 
