86c3616043325f706880a4051065a3adc729951b
ankitsejwal <ankit@sejw.al>
2020-10-16 23:56:38 +1100

    Palindrome lab program

---
+++ src/com/teachjava/labs/Palindrome.java
@@ -0,0 +1,33 @@
+package com.teachjava.labs;
+
+public class Palindrome {
+
+    // A palindrome is a word, phrase, or sentence reads the same backward or forward
+    //  Palindrome Examples:
+    //    pop
+    //    deed
+    //    kayak
+    //    civic
+    //    radar
+    //    level
+    //    deified
+    //    rotator
+    public static void main(String[] args) {
+
+        String word = "madam";
+        String reverse = "";
+
+        // reversing the word to form the String reverse
+        for( int counter = word.length() - 1; counter >= 0; counter -- ){
+            reverse += word.charAt(counter);
+        }
+
+        // check if reverse == word
+        if ( reverse.equals(word)){
+            System.out.println(word + " is a palindrome");
+        }
+        else{
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+}
