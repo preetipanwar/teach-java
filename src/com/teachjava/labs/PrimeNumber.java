b6257073aa7ea05a75479571d32e52b6a81a5176
ankitsejwal <ankit@sejw.al>
2020-10-17 02:18:09 +1100

    refactor PrimeNumber program

--- src/com/teachjava/labs/PrimeNumber.java
+++ src/com/teachjava/labs/PrimeNumber.java
@@ -31,21 +31,42 @@ public class PrimeNumber {
     }
 }
 
+// Actual lab program.
+class PrimeNumberInput {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Print prime numbers up-to: ");
+        int lastDividend =  input.nextInt();
+
+        // loop through all the dividends
+        for (int dividend = 2; dividend <= lastDividend; dividend++){
+
+            // Check if a dividend is Prime
+            for (int divisor = 2; divisor <= dividend; divisor++){
+                int quotient = dividend / divisor;
+                int remainder = dividend % divisor;
+
+                // check if the dividend has factors
+                // formula: dividend    = divisor   x quotient  + remainder
+                // e.g   :     12          = 2         x 6         + 0
+                // Here quotient is greater than 1 hence the number 12 can be
+                // divided into smaller parts (factors)
+
+                if (remainder == 0 && quotient > 1){
+                    // Not prime break the loop and go to the next dividend
+                    break;
+                }
+
+                // if dividend cannot be divided into any factors and loop
+                // reaches the final value which will surely divide the number
+                // by itself, then its a Prime number.
+                if (divisor == dividend){
+                    System.out.println(dividend + " is Prime");
+                }
+            } // end internal loop
+        } // end for loop for looping all the divisors one by one
 
 
-
-// dividend 6
-// divisor 2
-
-// quotient 3
-// remainder 0
-
-// dividend = divisor x quotient + remainder
-// 6 = 2 x 3 + 0
-// 8 = 2 x 2 + 0
-// 11 = 2 x 5 + 1
-// 11 = 11 x 1 + 0
-// 13 = 3 x 4 + 1
-
-// Prime - divisible by self and 1
-// Factor
+    } // end main()
+} // end Class
